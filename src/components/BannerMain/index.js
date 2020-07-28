2596fffb4edd3557dc7770bfef32a505e0acbea9
Andreferraz20 <luizlopesferraz22@gmail.com>
2020-07-28 17:21:52 -0300

    First

---
+++ src/components/BannerMain/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import VideoIframeResponsive from './components/VideoIframeResponsive';
+import { BannerMainContainer, ContentAreaContainer, WatchButton } from './styles';
+
+
+
+export default function BannerMain({
+  videoTitle,
+  videoDescription,
+  url,
+  backgroundimage,
+}) {
+  const youTubeID = getYouTubeId(url);
+  const bgUrl = backgroundimage;
+
+  return (
+    <BannerMainContainer backgroundImage={bgUrl}>
+      <ContentAreaContainer>
+        <ContentAreaContainer.Item>
+          <ContentAreaContainer.Title>
+            {videoTitle}
+          </ContentAreaContainer.Title>
+
+          <ContentAreaContainer.Description>
+            {videoDescription}
+          </ContentAreaContainer.Description>
+        </ContentAreaContainer.Item>
+
+        <ContentAreaContainer.Item>
+          <VideoIframeResponsive
+            youtubeID={youTubeID}
+          />
+          <WatchButton>
+            Assistir
+          </WatchButton>
+        </ContentAreaContainer.Item>
+      </ContentAreaContainer>
+    </BannerMainContainer>
+  );
+}
